b6b5e6769c92476a2c74d9459ce37c3e10e442e6
mushi333 <mujtaba.zahidi24@gmail.com>
2024-01-04 20:04:41 +1100

    Added in basic Jest testing

---
+++ __test__/page.test.tsx
@@ -0,0 +1,14 @@
+import '@testing-library/jest-dom'
+import { getByAltText, render } from '@testing-library/react'
+import Page from '../src/app/page'
+ 
+describe('Page', () => {
+  it('renders a heading', () => {
+    const { getByAltText } = render(<Page />)
+ 
+    const imageElement = getByAltText('Vercel Logo')
+    
+    expect(imageElement).toBeInTheDocument()
+    expect(imageElement).toHaveAttribute('src', '/vercel.svg')
+  })
+})
